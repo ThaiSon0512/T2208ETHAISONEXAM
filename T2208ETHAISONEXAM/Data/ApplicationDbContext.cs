412ea92ccac7de2a87d46d51ad450d9dc44bff65
SON <thaisonqwe@gmail.com>
2023-12-08 22:47:32 +0700

    EXAM

---
+++ T2208ETHAISONEXAM/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using T2208ETHAISONEXAM.Models;
+
+namespace T2208ETHAISONEXAM.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+		public DbSet<T2208ETHAISONEXAM.Models.Department>? Department { get; set; }
+		public DbSet<T2208ETHAISONEXAM.Models.Employee>? Employee { get; set; }
+	}
+}
---
+++ T2208ETHAISONEXAM/Models/Report.cs
@@ -0,0 +1,8 @@
+﻿namespace T2208ETHAISONEXAM.Models
+{
+    public class Report
+    {
+        public Department department { get; set; }
+        public List<Employee> employees { get; set; }
+    }
+}
